0533c1bf1e684425fd1a4609a8dddd820f7cef60
mariarosa8933h-code <mariarosa8933h@gmail.com>
2026-09-05 00:24:19 -0600

    últimos cambios de formulario.php

--- FilesPHP/formulario.php
+++ FilesPHP/formulario.php
@@ -12,16 +12,20 @@
         <label for="nombreMascota">Nombre de tu mascota: </label>
         <input type="text" id="nombreMascota" name="nombreMascota"><br>
         <!--Agrega otro LABEL para el color de mascota-->
-        <!--FOR debe compartir el nombre con ID y NAME en INPUT-->
-
         <!-- Agrega otro INPUT para el color de la mascota-->
-        <!--TYPE debe ser de tipo texto, ID y NAME compartirán el mismo nombre que en FOR en LABEL-->
+         <label for="colorMascota">Color de tu mascota: </label>
+         <input type="text" id="colorMascota" name="colorMascota"><br>
 
+        <!--TYPE debe ser de tipo texto, ID y NAME compartirán el mismo nombre que en FOR en LABEL-->
         <!--Agrega otro LABEL para la especia de la mascota-->
         <!--FOR debe compartir el nombre con ID y NAME en INPUT-->
+        <label for="especieMascota">Especie de tu mascota: </label>
+        <input type="text" id="especieMascota" name="especieMascota"><br>
 
         <!-- Agrega otro INPUT para la especia de la mascota-->
         <!--TYPE debe ser de tipo texto, ID y NAME compartirán el mismo nombre que en FOR en LABEL-->
+        <label for="habitatMascota">Habitat de tu Mascota: </label>
+        <input type="text" id="habitatMascota" name="habitatMascota"><br>
 
     </form>
 </body>
